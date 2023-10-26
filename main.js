6b44d403be61530a5329468c0fbc103b477c78e4
kurosiroke <129666467+kurosiroke@users.noreply.github.com>
2023-10-26 12:04:28 +0000

    update

---
+++ main.js
@@ -0,0 +1,19 @@
+import { View } from './js/view.js'
+
+export const Main = {
+  stage_id : 'NumberPlace',
+}
+
+function init(){
+  Main.view = new View()
+}
+
+switch (document.readyState) {
+  case 'complete':
+  case 'interactive':
+    init()
+    break
+  default:
+    window.addEventListener('DOMContetLoaded' , init)
+    break
+}
